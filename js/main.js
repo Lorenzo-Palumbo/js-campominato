df862c0cda568dfa0abc26dae39614d0b52654ea
Unknown <lorenzo.palumbo@hotmail.com>
2020-02-26 21:45:07 +0100

    completo

--- js/main.js
+++ js/main.js
@@ -1,39 +1,46 @@
-var numeriGenerati = [];
-
-
-while (numeriGenerati.length < 16) {
-    var numeroCasuale = generaRandomMinMax(1, 100);
-    if (!numeriGenerati.includes(numeroCasuale)) {
-        numeriGenerati.push(numeroCasuale)
-    }
-}
+var numeriGenerati = numeriCasualiInArray(16, 100);
 console.log(numeriGenerati);
-
-
-var numeroInserito = parseInt(prompt('Inserire numero da 1 a 100'));
-var numeroControllato = controlloNumeri(numeriGenerati, numeroInserito)
 var numeriUtente = [];
 
-/*
-function controlloNumeri(listaNumeri, numero){
-    var trovato = false;
-    for (var i = 0; i < listaNumeri.length; i++) {
-        if (listaNumeri[i] == numero) {
-            trovato = true;
-        }
-    }
-    return trovato
-}
-console.log(controlloNumeri(numeriGenerati, numeroInserito));
-*/
+//genero un ciclo che finisce o quando vinco o quando perdo
+//- vinco quando : quando numeriUtente == 84
+//- perdo quando : quando trovo una mina! => gameover = false => finche è false vado avanti..
 
-function inserimentoInArrayNumeriUtente(){
-    while (numeriUtente.length < 84 || !numeriUtente.includes(numeroInserito)) {
-        var numeroInserito = parseInt(prompt('Inserire numero da 1 a 100'));
+//Dentro il ciclo
+//- il numero è già stato inserito? se si non faccio nulla e richiederà un altro numero, altrimenti vado avanti
+//- il numero è una delle mine? se si gameover = true, altrimenti aggiungo il numero dentro numeriUtente
+var bomba = false
+while (numeriUtente.length + 1 < 85 && bomba == false) {
+    var numeroInserito = parseInt(prompt('Inserire numero da 1 a 100'));
+    if (numeriUtente.includes(numeroInserito) || numeroInserito < 1 || numeroInserito > 100) {
+        alert('Hai già inserito questo numero oppure non è compreso tra 1 e 100')
+    } else if (numeriGenerati.includes(numeroInserito)) {
+        console.log('Hai perso');
+        bomba = true
+    } else if (numeriUtente.length == 84) {
+        console.log('Hai vinto');
+    } else {
         numeriUtente.push(numeroInserito);
     }
 }
 
+//Scopriamo se ho vinto o perso..
+//- se numeriUtente == 84 allora ho vinto!
+//- se ho perso : gameover sarà true
+
+console.log(numeriUtente);
+
+
+function numeriCasualiInArray(numTotaleGenerati, maxNumeri) {
+    var numeriGenerati = [];
+    while (numeriGenerati.length < numTotaleGenerati) {
+        var numeroCasuale = generaRandomMinMax(1, 100);
+        if (!numeriGenerati.includes(numeroCasuale)) {
+            numeriGenerati.push(numeroCasuale);
+        }
+    }
+    return numeriGenerati
+}
 
 function generaRandomMinMax(min, max){
     var numeroRandom = Math.floor(Math.random() * (max - min + 1)) + min;
